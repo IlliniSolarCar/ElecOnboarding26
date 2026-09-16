4b937f59ccfef7fc4e9d15ce74d86ca25cf1baf4
Aaron Chen <aaronc14@illinois.edu>
2026-09-15 20:03:35 -0500

    do KiCad and LED projects

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -25,7 +25,9 @@
 #define P_LED2 P0_5
 #define P_LED3 P0_6
 #define P_LED4 P0_7
+#define P_LED P0_4
 // PROJECT 1 - You can define a pin macro here
+
 
 /*
  * COMMON PIN OBJECT DECLARATIONS
@@ -36,7 +38,7 @@ extern DigitalOut led2;
 extern DigitalOut led3;
 extern DigitalOut led4;
 // PROJECT 1 - You can declare a DigitalOut object here
-
+DigitalOut led_test(P_LED);
 /*
  * BOARD SPECIFIC PINS
  */
--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -96,7 +96,9 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+
+        led_test = !led_test.read();
+
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
