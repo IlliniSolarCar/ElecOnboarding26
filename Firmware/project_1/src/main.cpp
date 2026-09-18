6339be3124027a175fceacddfd51dff858e68941
CL-YQLM <145878564+CL-YQLM@users.noreply.github.com>
2026-09-18 00:19:47 -0500

    Project 1: blink LED on P0_4 at 1Hz

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,6 +98,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	led = !led; //allows it to toggle
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
