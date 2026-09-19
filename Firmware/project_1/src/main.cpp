cf40d045c5cce059954161657039a268bd467a3d
Giancarloggg <ggril@illinois.edu>
2026-09-19 17:28:41 -0500

    Project 1: blink LED on P0_4

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -4,7 +4,7 @@
  */
 
 #include <mbed.h>
-// PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -96,8 +96,9 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+            led5 = !led5;
         }
+        	//PROJECT 1 - add code here to actually make the LED blink
 
         //PROJECT 2 - use the potentiometer to change the blink rate
 
