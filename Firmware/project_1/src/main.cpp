0bbdeb0e10bf2d9026947b446865ea17f77cef83
wmans01 <wmans0001@gmail.com>
2026-09-21 16:48:35 -0500

    Completed Heartbeat Project

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,8 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
+
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -95,8 +97,9 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        if(timing.tickThreshold(last_task_1_time, HEARTBEAT_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+			heartbeat = !heartbeat;
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
