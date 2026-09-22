5fd7bb2f0fd57b0abd69412936b88031bfd129ef
anafernandez <anaferna101@gmail.com>
2026-09-22 09:59:51 -0500

    Completed onboarding projects 1 and 2

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -4,7 +4,6 @@
  */
 
 #include <mbed.h>
-// PROJECT 1 - Include something here!
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -96,7 +95,7 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+        	led_project.write(!led_project.read());
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
