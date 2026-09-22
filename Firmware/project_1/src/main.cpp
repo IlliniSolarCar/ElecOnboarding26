8cc391bb265e8372735a3eaca708c0cf472b480f
toemayto <austinshuuu@gmail.com>
2026-09-22 18:09:32 -0500

    LED Project Fix, Firmware Project

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -97,6 +97,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	led_test.write((led_test.read() + 1) % 2);
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
