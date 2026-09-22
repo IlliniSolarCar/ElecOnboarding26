b29f4d3791b9198a2400c49fc00770328135bbc7
THISISPANES <shimon.arai@outlook.com>
2026-09-22 16:40:04 -0500

    Firmware project completed

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,8 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "pins.h"
+#include "setup.h"
 
 
 /*
@@ -97,6 +99,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	heartbeat_led = !heartbeat_led;
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
