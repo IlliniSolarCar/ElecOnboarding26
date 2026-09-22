3a53b922061c93c7af24f848e6d4a87679d10111
adamk-299 <akaszok2@illinois.edu>
2026-09-22 10:10:59 -0500

    Revert "adkang2 LED project"

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,7 +5,6 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
-#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -98,9 +97,8 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
-            heartbeatLED.write(!heartbeatLED.read());
-
         }
+
         //PROJECT 2 - use the potentiometer to change the blink rate
 
 
