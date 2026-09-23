97ef8e3e616b9785b6edf8673fb93cc440e74a90
Saketh Satti <sakethsatti@wirelessprv-10-192-102-90.near.illinois.edu>
2026-09-23 11:49:31 -0500

    firmware project

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include <pins.h>
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,6 +98,8 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	led_test.write(led_test.read() ? 0 : 1);
+
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
