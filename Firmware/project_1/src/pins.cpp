ca12c5d6b541aaa795f85a2c72f7137cea0e8af6
RowanWilliams <rowanpw9@gmail.com>
2026-09-23 11:15:00 -0500

    t

--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -22,4 +22,7 @@ DigitalOut led1(P_LED1);
 DigitalOut led2(P_LED2);
 DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
+DigitalOut blinkLed(P_BLINK_LED);
+    blinkLed = 1;
+    blinkLed = 0;
 // PROJECT 1 - You can instantiate your DigitalOut object here
