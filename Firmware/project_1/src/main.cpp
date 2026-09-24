e65a2650645e591040cb53ca1c87b754b593f479
ilyliang <lyliang43273@gmail.com>
2026-09-23 21:00:47 -0500

    easy firmware

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -6,6 +6,8 @@
 #include <mbed.h>
 // PROJECT 1 - Include something here!
 #include "peripherals.h"
+#include "pins.h"
+#include "setup.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
@@ -97,6 +99,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	ProjectLED = !ProjectLED;
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
