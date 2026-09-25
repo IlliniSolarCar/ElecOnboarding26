fb283c754ed4fff94b16a0b7827a9742a5b1d27d
ck07022 <jeannenkim0202@gmail.com>
2026-09-24 22:21:36 -0500

    Complete firmware project 1 heartbeat

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -4,7 +4,7 @@
  */
 
 #include <mbed.h>
-// PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -96,7 +96,7 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+        	project1_led.write(!project1_led.read());
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
