4b91b1c278da9b023dfda02c25f5b9cd517e2c4f
Steven Coulson <coulson5@illinois.edu>
2026-09-25 13:56:38 -0500

    Firemware Project 1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -96,7 +97,7 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+        	heartbeat_led.write(!heartbeat_led.read());
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
