43af9348e591b7886f0d88f84cf1a56bd256d7b7
Omar Soliman <osoli6@illinois.edu>
2026-09-25 23:28:30 -0500

    Completed firmware project 1 (Heartbeat)

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,7 +10,8 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
-
+#include "pins.h"
+#include "setup.h"
 
 /*
  * This is an example function. It blinks the heartbeat LED and sends
@@ -97,6 +98,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	proj1_led = !proj1_led;
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
